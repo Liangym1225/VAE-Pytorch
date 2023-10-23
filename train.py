5e0255a189904d8af45cc68e8e95ba278eef0bba
liangym_greentes <liangym1225@gmail.com>
2023-10-23 13:15:17 +0900

    support image logging

--- train.py
+++ train.py
@@ -11,22 +11,22 @@ from utils import *
 import wandb
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--data',type=str,default="MNIST")
-parser.add_argument('--epochs',type=int,default=10)
-parser.add_argument('--batch_size',type=int,default=128)
-parser.add_argument('--distribution',type=str,default='gaussian')
-parser.add_argument('-lr','--learning_rate',type=float,default=1e-3)
-parser.add_argument('--latent_dimension',type=int,default=2)
-parser.add_argument('--logging',type=str,default="None")
-args=parser.parse_args()
+parser.add_argument("--data", type=str, default="MNIST")
+parser.add_argument("--epochs", type=int, default=10)
+parser.add_argument("--batch_size", type=int, default=128)
+parser.add_argument("--distribution", type=str, default="gaussian")
+parser.add_argument("-lr", "--learning_rate", type=float, default=1e-3)
+parser.add_argument("--latent_dimension", type=int, default=2)
+parser.add_argument("--logging", type=str, default="None")
+args = parser.parse_args()
 
-config={
-    "epochs":args.epochs,
-    "batch_size":args.batch_size,
-    "distribution":args.distribution,
-    "learning_rate":args.learning_rate,
-    "latent_dimension":args.latent_dimension,
-    "logging":args.logging
+config = {
+    "epochs": args.epochs,
+    "batch_size": args.batch_size,
+    "distribution": args.distribution,
+    "learning_rate": args.learning_rate,
+    "latent_dimension": args.latent_dimension,
+    "logging": args.logging,
 }
 
 config = VAEConfig(**config)
@@ -34,52 +34,58 @@ config.set_timestamp()
 config.save_config()
 
 if config.logging == "wandb":
-    run = wandb.init(
-        project="VAE",
-        dir=config.get_base_dir(),
-        config = config
-    )
+    run = wandb.init(project="VAE", dir=config.get_base_dir(), config=config)
 
-device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
- 
+device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+
 training_data = datasets.MNIST(
     root="./data", train=True, transform=transforms.ToTensor(), download=True
 )
-training_dataloader = DataLoader(training_data, batch_size=config.batch_size, shuffle=True)
+training_dataloader = DataLoader(
+    training_data, batch_size=config.batch_size, shuffle=True
+)
 
 vae = VAE(latent_dim=config.latent_dimension).to(device)
-optimizer = Adam(vae.parameters(),lr=config.learning_rate)
+optimizer = Adam(vae.parameters(), lr=config.learning_rate)
 loss_fn = ELBO(distribution=config.distribution, latent_dim=config.latent_dimension)
 
 num_train_batches = len(training_dataloader)
 epochs = config.epochs
-for e in trange(1,epochs+1, dynamic_ncols=True):
+for e in trange(1, epochs + 1, dynamic_ncols=True):
     vae.train()
-    train_loss_rc = 0.
-    train_loss_kl =0.
-    train_loss = 0.
-    for batch, (X,y) in enumerate(training_dataloader):
+    train_loss_rc = 0.0
+    train_loss_kl = 0.0
+    train_loss = 0.0
+    for batch, (X, y) in enumerate(training_dataloader):
         X = X.to(device)
         z, mu, logsig, output = vae(X)
-        loss, loss_rc, loss_kl=loss_fn(mu,logsig,output,X)
+        loss, loss_rc, loss_kl = loss_fn(mu, logsig, output, X)
 
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
         train_loss += loss.item()
-        train_loss_rc+=loss_rc
-        train_loss_kl+=loss_kl
+        train_loss_rc += loss_rc
+        train_loss_kl += loss_kl
 
     train_loss /= num_train_batches
     train_loss_rc /= num_train_batches
     train_loss_kl /= num_train_batches
-    tqdm.write(f"[TRAIN] Epoch: {e:3d}| Overall Loss: {train_loss:.2f}| Reconstruction Loss: {train_loss_rc:.2f}| KL Divergence: {train_loss_kl:.2f}")
+    tqdm.write(
+        f"[TRAIN] Epoch: {e:3d}| Overall Loss: {train_loss:.2f}| Reconstruction Loss: {train_loss_rc:.2f}| KL Divergence: {train_loss_kl:.2f}"
+    )
     if config.logging == "wandb":
-        run.log({
-            "Train/Overall Loss":train_loss,
-            "Train/Reconstruction Loss":train_loss_rc,
-            "Train/KL Divergence":train_loss_kl
-        })
+        run.log(
+            {
+                "Train/Overall Loss": train_loss,
+                "Train/Reconstruction Loss": train_loss_rc,
+                "Train/KL Divergence": train_loss_kl,
+                "Epochs": e,
+                "Random Sampling": wandb.Image(random_sampling(vae,2,28,5,device)),
+                "Manifold": wandb.Image(plot_manifold(vae,[-2,2],[-2,2],28,12,device)),
+                "Linear Interpolation": wandb.Image(plot_linear_interpolation(vae,[-2,2],[2,-2],28,25,device))
+            }
+        )
 
     """
     if e%10 ==0 and e!=0:
--- utils.py
+++ utils.py
@@ -4,67 +4,72 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 
-def random_sampling(model, latent_dim, epoch, num, root):
-    z = torch.randn(num**2,latent_dim)
+
+def random_sampling(model, latent_dim, img_size, num, device):
+    z = torch.randn(num**2, latent_dim).to(device)
     model.eval()
     with torch.no_grad():
         output = model.decoder(z)
-    
-    output.detach().numpy()
+
+    output=output.cpu().detach().numpy()
     output = np.squeeze(output)
-    plt.figure()
-    fig,ax = plt.subplots(nrows=5,ncols=5,figsize=(10,10))
-    plt.gray()
-    for i in range(num**2):
-        idx = divmod(i,5)
-        ax[idx].imshow(output[i])
-        ax[idx].axis('off')
-    
-    fig.savefig(os.path.join(root,"samples_"+str(epoch)+".png"))
-
-def plot_manifold(model,epoch, z0, z1, img_size, num, root):
-    img = np.zeros((num*img_size, num*img_size))
-    with torch.no_grad():
-        for i, y in enumerate(np.linspace(z1[0],z1[1],num)):
-            for j, x in enumerate(np.linspace(z0[0],z0[1],num)):
-                z = torch.Tensor(([[x,y]]))
-                output = model.decoder(z)
-                output = output.detach().numpy()
-                img[(num-1-i)*img_size:(num-i)*img_size, j*img_size:(j+1)*img_size]=output*255
-    
+    img = np.zeros((num * img_size, num * img_size))
+    for y in range(num):
+        for x in range(num):
+            idx = y * num + x
+            img[
+                y * img_size : (y + 1) * img_size, x * img_size : (x + 1) * img_size
+            ] = output[idx]*255
     img = Image.fromarray(img).convert("L")
-    img.save(os.path.join(root,"manifold_"+str(epoch)+".png"))
+    return img
 
 
-def plot_latent_space(model,epoch, dataloader, num_batches, root):
+def plot_manifold(model, z0, z1, img_size, num, device):
+    img = np.zeros((num * img_size, num * img_size))
+    model.eval()
+    with torch.no_grad():
+        for i, y in enumerate(np.linspace(z1[0], z1[1], num)):
+            for j, x in enumerate(np.linspace(z0[0], z0[1], num)):
+                z = torch.Tensor(([[x, y]])).to(device)
+                output = model.decoder(z)
+                output = output.cpu().detach().numpy()
+                img[
+                    (num - 1 - i) * img_size : (num - i) * img_size,
+                    j * img_size : (j + 1) * img_size,
+                ] = (
+                    output * 255
+                )
+
+    img = Image.fromarray(img).convert("L")
+    return img
+
+
+def plot_latent_space(model, epoch, dataloader, num_batches, root):
     plt.figure()
     model.eval()
     with torch.no_grad():
-        for i, (X,y) in enumerate(dataloader):
-            z,_,_,_=model(X)
-            z=z.detach().detach().numpy()
-            y=y.tolist()
-            plt.scatter(z[:,0],z[:,1],c=y,cmap='tab10')
+        for i, (X, y) in enumerate(dataloader):
 
-            if i>num_batches:
+            z, _, _, _ = model(X)
+            z = z.cpu().detach().numpy()
+            y = y.tolist()
+            plt.scatter(z[:, 0], z[:, 1], c=y, cmap="tab10")
+
+            if i > num_batches:
                 plt.colorbar()
-                plt.savefig(os.path.join(root,"latent_"+str(epoch)+".png"))
+                plt.savefig(os.path.join(root, "latent_" + str(epoch) + ".png"))
                 break
-            
-def plot_linear_interpolation(model, epoch, a,b,img_size, num, root):
-    img = np.zeros((img_size, num*img_size))
+
+
+def plot_linear_interpolation(model, a, b, img_size, num, device):
+    img = np.zeros((img_size, num * img_size))
+    model.eval()
     with torch.no_grad():
-        for i, alpha in enumerate(np.linspace(0,1,num)):
-            z = alpha*torch.Tensor(a)+(1-alpha)*torch.Tensor(b)
+        for i, alpha in enumerate(np.linspace(0, 1, num)):
+            z = (alpha * torch.Tensor(a) + (1 - alpha) * torch.Tensor(b)).to(device)
             output = model.decoder(z)
-            output = output.detach().numpy()
-            img[...,i*img_size:(i+1)*img_size]=output*255
-    
+            output = output.cpu().detach().numpy()
+            img[..., i * img_size : (i + 1) * img_size] = output * 255
+
     img = Image.fromarray(img).convert("L")
-    img.save(os.path.join(root, "interpolation_"+str(epoch)+".png"))
-
-
-
-
-
-
+    return img
